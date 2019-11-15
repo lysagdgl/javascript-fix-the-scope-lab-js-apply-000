15acebc5085be2c2c3596843e78f282e19c263b9
asynchronous-acrobat-1440 <lynn.x.liu@gmail.com>
2019-11-15 17:05:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-function theFunk(){
-  var funkyfunction = "FUNKY!"
-  return funkyfunction
+function funkyfunction(){
+  var theFunk = "FUNKY!"
+  return theFunk
 }
   
 
