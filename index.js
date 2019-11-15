bee0812dfc04e9b157648a693e24fc63e31a19b5
asynchronous-acrobat-1440 <lynn.x.liu@gmail.com>
2019-11-15 16:46:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-function funkyFunction = function() {
+var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
@@ -24,4 +24,6 @@ function funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction.String
+var theFunk = function(){
+  
+}
